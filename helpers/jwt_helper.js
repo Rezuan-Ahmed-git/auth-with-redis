a5036c31e89dc17435e9e892c94fd8bd32365495
rezuan <rezuan.ahmed.pro@gmail.com>
2023-10-12 14:45:23 +0600

    feat: generate refresh token

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -37,4 +37,23 @@ module.exports = {
       next();
     });
   },
+
+  signRefreshToken: (userId) => {
+    return new Promise((resolve, reject) => {
+      const payload = {};
+      const secret = process.env.REFRESH_TOKEN_SECRET;
+      const options = {
+        expiresIn: '1y',
+        issuer: 'safesealedtech.com',
+        audience: userId,
+      };
+      JWT.sign(payload, secret, options, (err, token) => {
+        if (err) {
+          console.log(err.message);
+          reject(createError.InternalServerError());
+        }
+        resolve(token);
+      });
+    });
+  },
 };
