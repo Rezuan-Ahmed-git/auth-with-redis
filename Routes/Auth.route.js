cb9f368d2b3969da5a5cb1488c114c25083af24a
rezuan <rezuan.ahmed.pro@gmail.com>
2023-10-12 15:14:34 +0600

    feat: refresh-token route handled

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -3,7 +3,11 @@ const bcrypt = require('bcrypt');
 const createError = require('http-errors');
 const User = require('../Models/User.model');
 const { authSchema } = require('../helpers/validation_schema');
-const { signAccessToken, signRefreshToken } = require('../helpers/jwt_helper');
+const {
+  signAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} = require('../helpers/jwt_helper');
 
 router.post('/register', async (req, res, next) => {
   try {
@@ -48,9 +52,23 @@ router.post('/login', async (req, res, next) => {
     next(error);
   }
 });
+
 router.post('/refresh-token', async (req, res, next) => {
-  res.send('refresh token route');
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) throw createError.BadRequest();
+
+    const userId = await verifyRefreshToken(refreshToken);
+
+    const accessToken = await signAccessToken(userId);
+    const rToken = await signRefreshToken(userId);
+
+    res.send({ accessToken, refreshToken: rToken });
+  } catch (error) {
+    next(error);
+  }
 });
+
 router.delete('/logout', async (req, res, next) => {
   res.send('logout route');
 });
