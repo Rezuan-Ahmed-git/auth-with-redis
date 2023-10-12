558b537a1e63cfdbd183d4d64de17954dd586ea4
rezuan <rezuan.ahmed.pro@gmail.com>
2023-10-12 12:25:06 +0600

    feat: login funtionality implemented

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const createError = require('http-errors');
 const User = require('../Models/User.model');
 const { authSchema } = require('../helpers/validation_schema');
@@ -23,8 +24,27 @@ router.post('/register', async (req, res, next) => {
     next(error);
   }
 });
+
 router.post('/login', async (req, res, next) => {
-  res.send('login route');
+  try {
+    const result = await authSchema.validateAsync(req.body);
+
+    const user = await User.findOne({ email: result.email });
+    if (!user) throw createError.NotFound('User not registered');
+
+    const isMatch = await bcrypt.compare(result.password, user.password);
+    if (!isMatch) throw createError.Unauthorized('Username/Password not valid');
+
+    const accessToken = await signAccessToken(user.id);
+
+    res.send({ accessToken });
+  } catch (error) {
+    if (error.isJoi === true) {
+      return next(createError.BadRequest('Invalid Username/Password'));
+    }
+
+    next(error);
+  }
 });
 router.post('/refresh-token', async (req, res, next) => {
   res.send('refresh token route');
